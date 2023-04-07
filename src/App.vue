60ff818d7d3bce07b41ff667df53692e13ec2800
shdangit <shdangit@gmail.com>
2023-04-07 20:51:09 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
-<template>
-  <h1>Helo vuejs</h1>
-</template>
-
 <script>
+import AppHeader from "@/components/AppHeader.vue";
 export default {
-  name: "App",
+  components: {
+    AppHeader,
+  },
 };
 </script>
-
+<template>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
 <style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
 </style>
---
+++ src/services/api.service.js
@@ -0,0 +1,12 @@
+const commonConfig = {
+    headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    },
+    };
+    export default (baseURL) => {
+    return axios.create({
+    baseURL,
+    ...commonConfig,
+    });
+    };
